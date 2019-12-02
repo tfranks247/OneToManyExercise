75034acb0f47bcf93132c2b837a01c3db07c6c6a
Tony Franks <tonyfranks@Tonys-MacBook-Pro.local>
2019-12-02 16:11:26 -0500

    buggin

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,9 +3,15 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Controller
@@ -15,7 +21,7 @@ public class HomeController {
     DirectorRepository directorRepository;
 
     @RequestMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
         Director director = new Director();
 
         director.setName("Steven Spielberg");
@@ -41,6 +47,41 @@ public class HomeController {
 
         model.addAttribute("directors", directorRepository.findAll());
         return "index";
+    }
 
+        @GetMapping("/add")
+        public String movieForm(Model model){
+            model.addAttribute("movie", new Movie());
+            model.addAttribute("directors", directorRepository.findAll());
+            return "movieform";
+    }
+
+
+    @PostMapping("/process")
+    public String processForm(@ModelAttribute movie movie1, @Valid Movie movie, BindingResult result){
+        if(result.hasErrors()){
+            return "movieform";
+        }
+        else
+            movieRepository.save();
+        return "redirect:/";
+    }
+
+
+
+    @GetMapping("/adddirector")
+    public String directorForm(Model model){
+        model.addAttribute("director", new Director());
+        return "directorform";
+    }
+    @PostMapping("/process")
+    public String processForm(@ModelAttribute director director1, @Valid Director director, BindingResult result){
+        if(result.hasErrors()){
+            return "directorform";
+        }
+        else
+            directorRepository.save(director);
+        return "redirect:/";
     }
 }
+
